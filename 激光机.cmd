34e327819148b4e355ad7b4635bd7e83d0428242
简 <44823507+jianpf@users.noreply.github.com>
2019-01-23 15:37:16 +0800

    Add files via upload

---
+++ 激光机.cmd
@@ -0,0 +1,23 @@
+@rem 20181017jianpf
+@echo off
+@title %1 "发送中..."
+@color c
+@rem 发送到位置
+set sendto=\\Pc201703201122\共享文件
+set log.txt=E:\Work\Temp\sendto.log.txt
+
+
+for /f %%i in ('date /t') do set odate=%%i
+for /f %%i in ('time /t') do set otime=%%i
+set odt=%odate% %otime%
+echo %odt% %1
+echo ...   
+
+copy %1 %sendto%  && echo %odate% %otime% %1 >%log.txt%.tmp && color a
+type %log.txt%>>%log.txt%.tmp
+type %log.txt%.tmp>%log.txt%
+pause 
+echo %odt% 发送时间  
+more %log.txt%
+echo %odt% 发送时间  
+pause
